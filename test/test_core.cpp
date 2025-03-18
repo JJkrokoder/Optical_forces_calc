1941674c4f6e3ffc3375d9381c515538f03c1359
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-03-18 10:57:39 +0100

    First test for optical forces formula DA

--- test/test_core.cpp
+++ test/test_core.cpp
@@ -5,14 +5,14 @@ using namespace OFC;
 TEST(CoreLibrary, SanityCheck) {
     // Test the sanity of the core library
     std::vector<std::complex<double>> dipole_moments = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    std::vector<std::complex<double>> field_gradients = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    std::vector<std::complex<double>> field_gradients = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     double epsilon_m = 1.0;
-    int num_particles = 1;
+    int num_particles = 2;
     int dimensions = 3;
-    std::vector<double> forces_array(3, 0.0);
+    std::vector<double> forces_array(num_particles*dimensions, 0.0);
     calculateOpticalForces(dipole_moments, field_gradients, epsilon_m, num_particles, dimensions, forces_array);
     EXPECT_EQ(forces_array[0], 0.5);
     EXPECT_EQ(forces_array[1], 0.0);
-    EXPECT_EQ(forces_array[2], 0.0);
+    EXPECT_EQ(forces_array[2], 0.5);
 }
 
