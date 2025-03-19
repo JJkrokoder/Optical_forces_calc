09b05749d2af0383c6f32435e9312d7fa281bb40
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-03-19 10:04:21 +0100

    Added co,pile-command file, so the vscode c++ error messages are solved

--- src/dielectric_optcoeff.cpp
+++ src/dielectric_optcoeff.cpp
@@ -1,9 +1,12 @@
 #include "dielectric_optcoeff.h"
 
+// Define the imaginary unit
+const std::complex<double> i_im(0.0, 1.0);
+
 namespace dioptco{
     
-    Permittivity::Permittivity(std::string material){
-        this->material = material;
+    Permittivity::Permittivity(std::string Material){
+        material = Material;
     }
 
     Permittivity::~Permittivity(){
@@ -11,7 +14,7 @@ namespace dioptco{
 
     std::complex<double> Permittivity::getPermittivity(double wavelength){
         
-        if (this->material == "gold"){
+        if (material == "gold"){
             double epsilon_b = 9;
             double h = 6.62607015;
             double c = 2.99792458;
@@ -20,9 +23,9 @@ namespace dioptco{
             double Ep = 9;
             double Et = 0.05;
 
-            return epsilon_b - (Ep * Ep / (E * E - 1j* E * Et));
+            return epsilon_b - (Ep * Ep / (E * E - i_im* E * Et));
         }
-        else if (this->material == "silver"){
+        else if (material == "silver"){
             return 2.0;
         }
         else{
